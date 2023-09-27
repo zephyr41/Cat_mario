1bd32ceee7310c6208b948b1a804e196f45d0ebe
Zephyr <118308029+zephyr41@users.noreply.github.com>
2023-09-27 21:06:11 +0200

    play.go

--- core/main.go
+++ core/main.go
@@ -3,17 +3,6 @@ package main
 
 
 func main() {
-	rl.InitWindow(1366, 768, "Donkey Kong Mario") // définit la taille de la fenetre
-	defer rl.CloseWindow() // définit les boutons pour être ouvert fermé ?
-	rl.SetTargetFPS(60) // définit les fps a x
-
-	for !rl.WindowShouldClose() { // boucle tant que c'est pas fermé
-		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-		rl.EndDrawing()
-	}
+	Play()
 	//firstx	
 }
