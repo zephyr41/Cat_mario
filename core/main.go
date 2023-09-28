3759cc5bedacc1a7f9dadd6258e326c696aa9f8c
Zephyr <118308029+zephyr41@users.noreply.github.com>
2023-09-28 20:08:48 +0200

    TryToSprite

--- core/main.go
+++ core/main.go
@@ -1,19 +1,39 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
 
+type ObjectWhoMoove struct {
+	Position rl.Vector2
+	txMarioSprite rl.Texture2D // faudrait dire en gros : la texture de 
+							   // de tel objet c'est ça
+							   // objectif, en affiché un simple
+}
 
-type window struct {
+type gameEngine struct {
 	width int32
 	heigh int32
 	title string
+	maxBarril int
+	score int
+	dead bool
+	mario ObjectWhoMoove
+	Barril ObjectWhoMoove
+	txSprites rl.Texture2D
+	isRunning bool
+
+
+
 }
 
-func main() {
-
-	var game window // défini game a la struct window
-	game.Init(1366, 768," Donkey Kong Mario") // initialise la fenetre : donc attribue les valeurs automatiquement ?
-	game.initGame()
+type texture struct {
 	
 }
+func main() {
 
+	var game gameEngine // défini game a la struct window
+	game.Init(1366, 768," Donkey Kong Mario", true, false, 0, ) // initialise la fenetre : donc attribue les valeurs automatiquement ?
+
+	game.initGame()
+
+}
 
