ee4057b59c35a80f73165fe93dbb8b9ab690cbf9
Zephyr <118308029+zephyr41@users.noreply.github.com>
2023-09-28 14:29:58 +0200

    Test

--- core/game.go
+++ core/game.go
@@ -10,13 +10,13 @@ func update() {
 	running = !rl.WindowShouldClose()
 }
 
-func (p *window) Init(width int32, height int32, title string) { // méthode = func par rapport a structure
+func (p *window) Init(width int32, height int32, title string) { // initialise les propriété de la fenetre 
 	p.width = width
 	p.heigh = height
 	p.title = title
 }
 
-func (p *window) initGame() { // permet de lancer la méthode (launcher)
+func (p *window) initGame() { // Initialise le jeu, en créant la fenêtre , 
 	rl.InitWindow(p.width, p.heigh, p.title) // définit la taille de la fenetre
 	defer rl.CloseWindow()                   // définit les boutons pour être ouvert fermé ?
 	rl.SetTargetFPS(60)                      // définit les fps a x
@@ -33,6 +33,7 @@ func render() {
 	rl.ClearBackground(rl.Black)
 	drawScene()
 	rl.EndDrawing()
+	
 }
 
 func quit() {
