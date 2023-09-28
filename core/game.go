e5acca2e473f5e3a7c65858aa76dd2dea33cbfb8
Zephyr <118308029+zephyr41@users.noreply.github.com>
2023-09-28 12:42:09 +0200

    PointerSetUp

--- core/game.go
+++ core/game.go
@@ -1,32 +1,43 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
-
-func input(){}
-func update(){}
-func render(){
-	rl.BeginDrawing()
-	rl.ClearBackground(rl.Black )
+var running = true // permet de savoir si la fenetre est ouverte ou fermer
+func input()       {}
+func update() {
+	running = !rl.WindowShouldClose()
 }
 
-func Play(w window) {
-	rl.InitWindow(w.width, w.heigh, "Donkey Kong Mario") // définit la taille de la fenetre
-	rl.InitWindow(w.width, w.heigh, w.title) // définit la taille de la fenetre
-	defer rl.CloseWindow() // définit les boutons pour être ouvert fermé ?
-	rl.SetTargetFPS(60) // définit les fps a x
-	for !rl.WindowShouldClose() { // boucle tant que c'est pas fermé
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-		rl.EndDrawing()
-	}
-}
-
-func (p *window) Init(width int32, height int32, isClose bool, title string){ // méthode = func par rapport a structure
+func (p *window) Init(width int32, height int32, title string) { // méthode = func par rapport a structure
 	p.width = width
 	p.heigh = height
-	p.isClose = isClose
 	p.title = title
 }
 
+func (p *window) initGame() { // permet de lancer la méthode (launcher)
+	rl.InitWindow(p.width, p.heigh, p.title) // définit la taille de la fenetre
+	defer rl.CloseWindow()                   // définit les boutons pour être ouvert fermé ?
+	rl.SetTargetFPS(60)                      // définit les fps a x
+	for running {
+		input()
+		update()
+		render()
+	}
+	quit()
+}
+
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Black)
+	drawScene()
+	rl.EndDrawing()
+}
+
+func quit() {
+	rl.CloseWindow()
+}
+
+func drawScene() {
+}
--- core/main.go
+++ core/main.go
@@ -5,20 +5,15 @@ package main
 type window struct {
 	width int32
 	heigh int32
-	isClose bool
 	title string
-
-
 }
 
 func main() {
 
 	var game window // défini game a la struct window
-	game.Init(1366, 768, true," Donkey Kong Mario") // initialise la fenetre : donc attribue les valeurs automatiquement ?
-	var menu window
-	menu.Init(1366, 768, true, "Donkey Kong Mario")
-	Play(game) // envoie la fenêtre initialisé ?
-
+	game.Init(1366, 768," Donkey Kong Mario") // initialise la fenetre : donc attribue les valeurs automatiquement ?
+	game.initGame()
+	
 }
 
 
