166a973fcc672812a3ad8176d93045328fddfdf9
M1ck3y-ru <clement.garciaspro@gmail.com>
2023-09-29 11:16:32 +0200

    game

--- core/game.go
+++ core/game.go
@@ -7,8 +7,6 @@ import (
 
 //var running = true // permet de savoir si la fenetre est ouverte ou fermer
 //var groundTexture rl.Texture2D
-
-
 func (p *gameEngine) Init(width int32, height int32, title string, isRunning bool, dead bool, score int, ) { // initialise les propriété de la fenetre
 	p.width = width
 	p.heigh = height
@@ -17,6 +15,7 @@ func (p *gameEngine) Init(width int32, height int32, title string, isRunning boo
 	p.dead = dead
 	p.score = score
 }
+
 
 func (p *gameEngine) initGame() { // Initialise le jeu, en créant la fenêtre ,
 	rl.InitWindow(p.width, p.heigh, p.title) // définit la taille de la fenetre
@@ -40,7 +39,6 @@ func (p *gameEngine) load(){ // méthode pour load tous les assets
 
 func input() {
 
-
 }
 
 func update(w *gameEngine) { // va définir les mouvements du personnage
@@ -53,32 +51,14 @@ rl.BeginDrawing()
 rl.ClearBackground(rl.Black)
 rl.LoadTexture("assets/Allsprites.png")
  // faire une condition pour dire tant que le joueur n'est pas mort : 
-	//rl.DrawTexture(g.TxSprites, g.frameRec)
-	rl.EndDrawing()
-	
-	rl.DrawRectangle(650,100,100,38,rl.Blue)
-
-	rl.DrawRectangle(650,235,100,38,rl.Yellow)
-
-	rl.DrawRectangle(650,500,100,38,rl.Red)
-
 //le debut de la mort des bouttons
-
-	if rl.IsMouseButtonPressed(0){
-		rl.DrawRectangle(650,100,150,80,rl.Purple)
-	}
-	if rl.IsMouseButtonPressed(0){
-		rl.DrawRectangle(650,235,150,80,rl.Purple)
-	}
-	if rl.IsMouseButtonPressed(0){
-		rl.DrawRectangle(650,500,150,80,rl.Purple)
-	}
 }
+//________________________________________________________________________________//
+//________________________________________________________________________________//
 
 func DrawTexturePro(texture rl.Texture2D, sourceRec, destRec rl.Rectangle, origin rl.Vector2, rotation float32, tint rl.Color) { // permet de faire un carrée pour afficher un sprite : 
-
 }
+
 func quit() {
 	rl.CloseWindow()
 }
-
