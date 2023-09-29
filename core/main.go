6a96fbf60be062484d22a336fa952d9669f8494b
Zephyr <118308029+zephyr41@users.noreply.github.com>
2023-09-29 21:21:34 +0200

    TestSound

--- core/main.go
+++ core/main.go
@@ -1,43 +1,74 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
-
-type ObjectWhoMoove struct {
-	Position rl.Vector2
-		
-							   // de tel objet c'est ça
-							   // objectif, en affiché un simple
-}
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type gameEngine struct {
-	width int32
-	heigh int32
-	title string
-	maxBarril int
-	score int
-	dead bool
-	mario ObjectWhoMoove
-	//barril ObjectWhoMoove
-	texture rl.Texture2D
-	playerSrc rl.Rectangle
-	playerDest rl.Rectangle
-	playerVector rl.Vector2
-	playerSpeed float32
-	
-	isRunning bool
-
-
-
+	width      int32
+	height     int32
+	title      string
+	isRunning  bool
+	dead       bool
+	score      int
+	texture    rl.Texture2D
+	musicMenu  rl.Music
+	// Add other properties as needed
 }
 
+func (p *gameEngine) Init(width, height int32, title string, isRunning, dead bool, score int) {
+	p.width = width
+	p.height = height
+	p.title = title
+	p.isRunning = isRunning
+	p.dead = dead
+	p.score = score
+}
+
+func (p *gameEngine) initGame() {
+	rl.InitWindow(p.width, p.height, p.title)
+	rl.InitAudioDevice()
+	rl.SetMasterVolume(1)
+
+	p.isRunning = true
+	p.texture = rl.LoadTexture("../assets/assets.png")
+
+	// Load and play the music
+	p.musicMenu = rl.LoadMusicStream("../audio/music/menu.mp3")
+	rl.PlayMusicStream(p.musicMenu)
+
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+}
+
+func (p *gameEngine) update() {
+	rl.UpdateMusicStream(p.musicMenu)
+}
+
+func (p *gameEngine) render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.RayWhite)
+	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+	rl.EndDrawing()
+}
+
+func (p *gameEngine) quit() {
+	rl.UnloadTexture(p.texture)
+	rl.UnloadMusicStream(p.musicMenu)
+	rl.CloseAudioDevice()
+	rl.CloseWindow()
+}
 
 func main() {
+	var engine gameEngine
 
-	var game gameEngine // défini game a la struct window
-	game.Init(1366, 768," Donkey Kong Mario", true, false, 0) // initialise la fenetre : donc attribue les valeurs automatiquement ?
-	// qu'est game.mario ?????????
+	engine.Init(800, 600, "My Game", false, false, 0)
+	engine.initGame()
 
-	game.initGame()
+	for engine.isRunning {
+		engine.update()
+		engine.render()
+	}
 
+	engine.quit()
 }
-
